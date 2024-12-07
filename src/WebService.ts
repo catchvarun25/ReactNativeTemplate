15d7b0f84722a87791524ddd6744ceeb681f045e
Varun Mehta <catchvarun25@gmail.com>
2024-12-07 13:31:36 +0800

    Integrated react-native-config for configuration settings management.

--- src/WebService.ts
+++ src/WebService.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Config from "react-native-config";
 
 export enum REST_METHODS {
   GET,
@@ -7,16 +8,42 @@ export enum REST_METHODS {
 }
 
 export enum END_POINT {
-  TOP_HEADLINES = "https://newsapi.org/v2/top-headlines",
+  TOP_HEADLINES = "/v2/top-headlines",
 }
 
-export function callRestAPI(url: string, method: REST_METHODS, payload?: any) {
+const articleAxios = axios.create({
+  baseURL: END_POINT.TOP_HEADLINES,
+  timeout: 10000,
+});
+
+articleAxios.interceptors.request.use(
+  (config) => {
+    const token = Config.ACCESS_TOKEN;
+    if (token) {
+      config.headers["X-Api-Key"] = token;
+    }
+    console.log("Request Config:", config);
+    return config;
+  },
+  (error) => {
+    // Handle request errors
+    console.error("Request Error:", error);
+    return Promise.reject(error);
+  }
+);
+
+export function callRestAPI(
+  endpoint: string,
+  method: REST_METHODS,
+  payload?: any
+) {
+  const url = Config.API_HOST_URL + endpoint;
   switch (method) {
     case REST_METHODS.GET:
-      return axios.get(url);
+      return articleAxios.get(url, { params: payload });
     case REST_METHODS.POST:
-      return axios.post(url, payload);
+      return articleAxios.post(url, payload);
     case REST_METHODS.PUT:
-      return axios.put(url, payload);
+      return articleAxios.put(url, payload);
   }
 }
