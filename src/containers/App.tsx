2413aa1e50658258dd9a9b3c07cf72544de0f094
Varun Mehta <catchvarun25@gmail.com>
2025-08-26 22:04:00 +0800

    Updated MMKV to work with Simulator

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -4,29 +4,10 @@ import store from "../store"; // Adjust the path to your store
 import AppNavigator from "../navigations/AppNavigator"; // Your app's navigation
 import { GestureHandlerRootView } from "react-native-gesture-handler"; //For managing gestures in app
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
-import { DevSettings, NativeModules } from "react-native";
 import { I18nextProvider } from "react-i18next";
 import i18n from "../../i18n";
 
 const App = () => {
-  // eslint-disable-next-line require-await
-  const main = async () => {
-    const message = {
-      stop: "Stop Debugging",
-      debug: "Debug JS Remotely",
-    };
-
-    DevSettings.addMenuItem(message.debug, () => {
-      NativeModules.DevSettings.setIsDebuggingRemotely(true);
-    });
-    DevSettings.addMenuItem(message.stop, () => {
-      NativeModules.DevSettings.setIsDebuggingRemotely(false);
-    });
-  };
-  setTimeout(async () => {
-    await main();
-  }, 200);
-
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <I18nextProvider i18n={i18n}>
